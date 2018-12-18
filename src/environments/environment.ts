ac6dbea4db65fb3c70d8f3009eb541f0842c0c79
Vilash Varghese <vilashinparadise@gmail.com>
2018-12-18 20:55:49 +0530

    minor bug fixes.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     production: false,
     maxCachedForecastAge: 1000 * 60 * 10,  // 10 minutes
     openWeatherMap: {
-        apiUrl: 'http://api.openweathermap.org/data/2.5/forecast',
+        apiUrl: 'https://api.openweathermap.org/data/2.5/forecast',
         apiKey: '6d67e9fa583373e16891628f2392a2c1'
     }
 };
